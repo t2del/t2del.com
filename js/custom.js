feb5be2cdb2301b91dff0a47c3298c91c545282c
Dennis Macapagal <macapagal.dennis@gmail.com>
2023-01-19 17:49:20 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,6 @@
 
 const obeserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
