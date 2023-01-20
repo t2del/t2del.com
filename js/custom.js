c4e7fc3cdcc1d2576381e196f907ab164f497d8e
Dennis Macapagal <macapagal.dennis@gmail.com>
2023-01-20 17:44:16 +0800

    added scrollbar animation

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,39 @@ const obeserver = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.notshow');
 hiddenElements.forEach((el) => obeserver.observe(el));
+
+
+// import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
+// const scrollTracker = document.querySelector('.scroll-tracker');
+// const scrollTrakingTimeline = new ScrollTimeline({
+//     source: document.scrollingElement,
+//     orientation: "block",
+//     scrollOffsets: [CSS.percent(0), CSS.percent(100)]
+// });
+
+// scrollTracker.animate(
+//     {
+//         transform: ["scaleX(0)", "scaleX(1)"]
+//     },
+//     {
+//         duration: 1,
+//         timeiline: scrollTrakingTimeline,
+//     }
+// );
+
+$(document).scroll(function (e) {
+    var scrollAmount = $(window).scrollTop();
+    var documentHeight = $(document).height();
+    var windowHeight = $(window).height();
+    var scrollPercent = (scrollAmount / (documentHeight - windowHeight)) * 100;
+    var roundScroll = Math.round(scrollPercent);
+    
+    // For scrollbar 1
+    $(".scrollBar1").css("width", scrollPercent + "%");
+    $(".scrollBar1 span").text(roundScroll);
+    
+    // // For scrollbar 2
+    // $(".scrollBar2").css("height", scrollPercent + "%");
+    // $(".scrollBar2 span").text(roundScroll);
+});
