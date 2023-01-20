c5928b515db7831ce741605eed9cd1580a17c88a
t2del <macapagal.dennis@gmail.com>
2023-01-21 00:45:26 +0800

    added scroll to top animation

--- js/custom.js
+++ js/custom.js
@@ -21,25 +21,6 @@ const hiddenElements = document.querySelectorAll('.notshow');
 hiddenElements.forEach((el) => obeserver.observe(el));
 
 
-// import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
-
-// const scrollTracker = document.querySelector('.scroll-tracker');
-// const scrollTrakingTimeline = new ScrollTimeline({
-//     source: document.scrollingElement,
-//     orientation: "block",
-//     scrollOffsets: [CSS.percent(0), CSS.percent(100)]
-// });
-
-// scrollTracker.animate(
-//     {
-//         transform: ["scaleX(0)", "scaleX(1)"]
-//     },
-//     {
-//         duration: 1,
-//         timeiline: scrollTrakingTimeline,
-//     }
-// );
-
 $(document).scroll(function (e) {
     var scrollAmount = $(window).scrollTop();
     var documentHeight = $(document).height();
@@ -50,8 +31,67 @@ $(document).scroll(function (e) {
     // For scrollbar 1
     $(".scrollBar1").css("width", scrollPercent + "%");
     $(".scrollBar1 span").text(roundScroll);
-    
-    // // For scrollbar 2
-    // $(".scrollBar2").css("height", scrollPercent + "%");
-    // $(".scrollBar2 span").text(roundScroll);
 });
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+jQuery(document).ready(function () {
+	$().UItoTop({
+		easingType: 'easeOutQuint'
+	});
+});
+
+(function ($) {
+	$.fn.UItoTop = function (options) {
+		var defaults = {
+			text: 'To Top',
+			min: 200,
+			inDelay: 600,
+			outDelay: 400,
+			containerID: 'ToTop',
+			containerHoverID: 'ToTopHover',
+			scrollSpeed: 800,
+			easingType: 'linear'
+		};
+		
+		var settings = $.extend(defaults, options);
+		var containerIDhash = '#' + settings.containerID;
+		var containerHoverIDHash = '#' + settings.containerHoverID;
+		$('body').append('<span id="' + settings.containerID + '">' + settings.text + '</span>');
+		$(containerIDhash).hide().click(function (event) {
+			$('html, body').animate({
+				scrollTop: 0
+			}, settings.scrollSpeed);
+			event.preventDefault();
+		})
+		.prepend('<span id="' + settings.containerHoverID + '"></span>')
+		.hover(function () {
+			$(containerHoverIDHash, this).stop().animate({
+				'opacity': 1
+			}, 600, 'linear');
+			}, function () {
+			$(containerHoverIDHash, this).stop().animate({
+				'opacity': 0
+			}, 700, 'linear');
+		});
+		
+		$(window).scroll(function () {
+			var sd = $(window).scrollTop();
+			if (typeof document.body.style.maxHeight === "undefined") {
+				$(containerIDhash).css({
+					'position': 'absolute',
+					'top': $(window).scrollTop() + $(window).height() - 50
+				});
+			}
+			if (sd > settings.min)
+			$(containerIDhash).fadeIn(settings.inDelay);
+			else
+			$(containerIDhash).fadeOut(settings.Outdelay);
+		});
+	};
+})(jQuery);
+
+function cl(x) {
+	console.log(x);
+}
--- js/template.js
+++ js/template.js
@@ -109,5 +109,6 @@
 			});
 		}
 
+
 	}); // End document ready
 })(this.jQuery);
