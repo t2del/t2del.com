9eeec870b59610079c4470ba532f4e762289945d
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-07-13 23:14:21 +0800

    upgrade astro, installed sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 // import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://t2del.github.io/',
+    site: 'https://www.t2del.com',
     base: '/',
+    integrations: [sitemap()],
     // output: 'server',
     // adapter: nodejs(),
 });
